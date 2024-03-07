73d54105f2c4ecd1173667a66bb0da377ec75266
amittawrej <amittawrej1999@gmail.com>
2024-03-07 18:35:06 +0530

    change in route 2

--- app.js
+++ app.js
@@ -1,34 +1,67 @@
-import express from "express";
+// import express from "express";
 
-import userRouter from "./routes/user.js"
-import taskRouter from './routes/task.js'
+// import userRouter from "./routes/user.js"
+// import taskRouter from './routes/task.js'
+// import { config } from "dotenv";
+// import cookieParser from "cookie-parser";
+// import { errorMiddleware } from "./middlewares/error.js";
+// import cors from "cors";
+// // import { createProxyMiddleware } from "http-proxy-middleware";
+// export const app=express();
+// config({
+//     path:'./data/config.env'
+// });
+// //using middleware
+// app.use(express.json());
+// app.use(cookieParser());
+// app.use(cors({
+//     origin:[process.env.FRONTEND_URL],
+//     methods:["GET","POST","PUT","DELETE"],
+//     credentials:true,
+// }))
+
+// app.use("/api/v1/users",userRouter);
+// app.use("/api/v1/tasks",taskRouter);
+
+
+// app.get('*', function(req, res) {
+//     res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
+//   });
+
+// // error middleware
+// app.use(errorMiddleware)
+
+import express from "express";
 import { config } from "dotenv";
 import cookieParser from "cookie-parser";
 import { errorMiddleware } from "./middlewares/error.js";
 import cors from "cors";
-// import { createProxyMiddleware } from "http-proxy-middleware";
-export const app=express();
+import { fileURLToPath } from 'url';
+import { dirname } from 'path';
+// import path from 'path';
+const __dirname = dirname(fileURLToPath(import.meta.url));
+
+export const app = express();
 config({
-    path:'./data/config.env'
+    path: './data/config.env'
 });
+
 //using middleware
 app.use(express.json());
 app.use(cookieParser());
 app.use(cors({
-    origin:[process.env.FRONTEND_URL],
-    methods:["GET","POST","PUT","DELETE"],
-    credentials:true,
-}))
+    origin: [process.env.FRONTEND_URL],
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    credentials: true,
+}));
 
-app.use("/api/v1/users",userRouter);
-app.use("/api/v1/tasks",taskRouter);
+// Serving static files from the 'todobyaat' directory
+app.use(express.static(__dirname + '/todobyaat'));
 
-
-app.get('*', (req, res) => {
-    res.redirect(process.env.FRONTEND_URL);
+// Wildcard route handler
+app.get('*', function(req, res) {
+    res.sendFile(__dirname + '/todobyaat/index.html');
 });
 
-// error middleware
-app.use(errorMiddleware)
-
-
+// Error middleware
+app.use(errorMiddleware);
